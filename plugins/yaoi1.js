96e8f384450e25ab74ca29c408cd9fc1ec59dbf7
AgromosSpboy <103408838+AgromosSpboy@users.noreply.github.com>
2022-04-17 21:12:26 -0400

    Update yaoi1.js

--- plugins/yaoi1.js
+++ plugins/yaoi1.js
@@ -3,6 +3,6 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
 heum = await fetch(`https://api-alc.herokuapp.com/api/nsfw/yaoi?&apikey=ConfuMods`)
 json = await heum.buffer()
-conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', 'agromos sp - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
+conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', 'sapito - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
 handler.command = /^(yaoi)$/i
 module.exports = handler
