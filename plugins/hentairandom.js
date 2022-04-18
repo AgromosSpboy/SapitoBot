f794efa3f60f3025512a981de53e06d4758ef3f9
AgromosSpboy <103408838+AgromosSpboy@users.noreply.github.com>
2022-04-17 21:20:36 -0400

    Update hentairandom.js

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -3,7 +3,7 @@ let fetch = require('node-fetch')
      if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Hentai agromos sp*', 'Agromos  SP- Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*Hentai asapitop*', 'Agromos  SP- Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(hentai|hentay)$/i
 
